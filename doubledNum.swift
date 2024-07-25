bbeac6efbcc9244762d6a225901106a9997ac981
CodeofGuff <guffcode@gmail.com>
2024-07-24 18:41:16 -0700

    We learning

---
+++ doubledNum.swift
@@ -0,0 +1,10 @@
+
+// Code as fast as you can! 
+// You need to double the integer and return it.
+
+
+func doubleInteger(_ num: Int) -> Int {
+  let doubledNum = num * 2
+  return doubledNum
+}
+print("Your'e number is doubled is \(doubleInteger(8))")
