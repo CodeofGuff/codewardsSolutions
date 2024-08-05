2d141e9bbb0a0b176571b647e080e776b90ec66b
CodeofGuff <guffcode@gmail.com>
2024-08-04 20:28:07 -0700

    Create convertBoolStr.swift

---
+++ convertBoolStr.swift
@@ -0,0 +1,13 @@
+
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, 
+// or a "No" string for false
+
+
+func boolToWord(_ bool: Bool) -> String {
+  if bool == true {
+    "Yes"
+  } else {
+    "No"
+  }
+}
