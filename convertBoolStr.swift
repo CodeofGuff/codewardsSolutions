9183205fa982f11d7772c515e567dd06f5c51e1d
guFF <161796631+guffxx@users.noreply.github.com>
2025-07-30 17:20:00 -0700

    spaces

--- convertBoolStr.swift
+++ convertBoolStr.swift
@@ -1,6 +1,4 @@
-
-
-// Complete the method that takes a boolean value and return a "Yes" string for true, 
+// Complete the method that takes a boolean value and return a "Yes" string for true,
 // or a "No" string for false
 
 func boolTooWord(_ bool: Bool) -> String {
--- differenceVolumeCuboids.swift
+++ differenceVolumeCuboids.swift
@@ -1,18 +1,13 @@
 //
 //  differenceVolumeCuboids.swift
-//  
+//
 //
 //  Created by David Guffre on 4/21/25.
 //  Multiply at the first index
 import Cocoa
 
 func findDifference(_ a: [Int], _ b: [Int]) -> Int {
-	abs(a.reduce(1, *) - b.reduce(1, *))
+  abs(a.reduce(1, *) - b.reduce(1, *))
 }
 
-
-
 findDifference([2, 2, 3], [5, 4, 1])
-
-
-
